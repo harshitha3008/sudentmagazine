5a9d3bedeb922122c0e5245cdf5926bfb5ccb82b
harshitha3008 <lingamaneniharshitha@gmail.com>
2025-04-15 01:25:27 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,10 @@ app.use("/api", magazineRoutes);
 app.use("/uploads", express.static("uploads"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
