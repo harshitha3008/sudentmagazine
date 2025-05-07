c268ffc3a103753bd6f3a7faef1a39e53e72a435
Harshitha Lingamaneni <164991405+harshitha3008@users.noreply.github.com>
2025-05-07 20:01:58 +0530

    Update magazineRoutes.js file size incremented

--- backend/routes/magazineRoutes.js
+++ backend/routes/magazineRoutes.js
@@ -32,7 +32,7 @@ const upload = multer({
       cb(null, `${Date.now()}_${file.originalname}`);
     }
   }),
-  limits: { fileSize: 10 * 1024 * 1024 },
+  limits: { fileSize: 200 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     if (file.mimetype === 'application/pdf') {
       cb(null, true);
